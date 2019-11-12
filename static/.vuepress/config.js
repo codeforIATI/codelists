da7be6135e416789aa8226bfb284d2ccccf8c447
Andy Lulham <a.lulham@gmail.com>
2019-11-12 07:02:01 +0000

    Add API to navbar

--- static/.vuepress/config.js
+++ static/.vuepress/config.js
@@ -3,8 +3,11 @@ var locales = JSON.parse(fs.readFileSync("docs/.vuepress/locales.json"))
 module.exports = {
   locales: locales,
   themeConfig: {
+    nav: [{
+      text: 'API',
+      link: '/api/'
+    }],
     locales: locales,
     logo: 'https://codeforiati.org/assets/img/logo.png?v=7ddc8d1394de72bd6ae00937b90158393b9bb996'
   }
 }
-
