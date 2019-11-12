61a06fb5d4ed32cfeed6b63331c4d22508dc7296
Andy Lulham <a.lulham@gmail.com>
2019-11-12 12:46:03 +0000

    Use an absolute link instead :(

--- static/.vuepress/config.js
+++ static/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   themeConfig: {
     nav: [{
       text: 'API',
-      link: '/api/'
+      link: 'https://codelists.codeforiati.org/api/'
     }],
     locales: locales,
     logo: 'https://codeforiati.org/assets/img/logo.png?v=7ddc8d1394de72bd6ae00937b90158393b9bb996'
