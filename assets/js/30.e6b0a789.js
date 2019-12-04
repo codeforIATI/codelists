8e1f9d193ff7ce8157ac5ee2a331d2ed4330eed4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-04 00:44:49 +0000

    Deploy codeforIATI/codelists to github.com/codeforIATI/codelists.git:gh-pages

---
+++ assets/js/30.e6b0a789.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{273:function(t,e,s){"use strict";s.r(e);var n=s(2),l=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("CodelistPage",{attrs:{codelist:"UNSDG-Goals",lang:"en"}})],1)}),[],!1,null,null,null);e.default=l.exports}}]);
