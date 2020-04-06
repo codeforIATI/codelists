1ef393f121853dd91f3945036f89a347e8aca1aa
Andy Lulham <a.lulham@gmail.com>
2020-04-06 21:43:48 +0100

    Logging

--- src/gen.sh
+++ src/gen.sh
@@ -32,13 +32,20 @@ done
 rm -rf docs
 cp -r static docs
 
+echo ' *** Copy source XML to CLv3 ***'
 mkdir -p docs/.vuepress/public/api/clv2/xml docs/.vuepress/public/api/clv3
 cp -r codelists docs/.vuepress/public/api/clv3/xml
+
+echo ' *** Convert CLv3 XML to CLv2 XML ***'
 for f in codelists/*; do
     python src/v3tov2.py $f > docs/.vuepress/public/api/clv2/xml/`basename $f`;
 done
+echo ' *** Generate CLv2 other formats (CSV; JSON; XLSX) ***'
 python src/gen.py docs/.vuepress/public/api/clv2
+echo ' *** Convert CLv2 all formats to CLv1 ***'
 python src/v2tov1.py docs/.vuepress/public/api/clv2 docs/.vuepress/public/api/clv1
+echo ' *** Copy CLv2 other formats (CSV; JSON; XLSX) to CLv3 ***'
 cp -r docs/.vuepress/public/api/clv2/{codelists.json,codelists.xml,csv,json,xlsx} docs/.vuepress/public/api/clv3/
 
+echo ' *** Copy Clv3 all formats to root ***'
 cp -r docs/.vuepress/public/api/clv3/* docs/.vuepress/public/api
