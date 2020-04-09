e93dfd2309d2fc3c299776c4d2d9b9c964eb2c51
Mark Brough <mark.brough@gmail.com>
2020-04-09 18:47:30 +0200

    Display GLIDENumber

--- src/gen.sh
+++ src/gen.sh
@@ -24,7 +24,7 @@ mkdir codelists
 for f in ${codelists[*]}; do
     cp IATI-Codelists-NonEmbedded/xml/$f.xml codelists
 done
-unofficial_codelists=("HumanitarianGlobalClusters" "RegionM49")
+unofficial_codelists=("HumanitarianGlobalClusters" "RegionM49" "GLIDENumber")
 for f in ${unofficial_codelists[*]}; do
     cp Unofficial-Codelists/xml/$f.xml codelists
 done
