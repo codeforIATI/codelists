20f743ff5c13110c0759691b0666430b762bb8a6
Andy Lulham <a.lulham@gmail.com>
2021-10-22 09:50:19 +0100

    Move gen_105 into gen

--- src/gen.sh
+++ src/gen.sh
@@ -28,6 +28,15 @@ else
     git clone --branch version-2.03 https://github.com/IATI/IATI-Codelists.git
 fi
 
+if [ -d IATI-Codelists-105 ]; then
+    cd IATI-Codelists-105 || exit 1
+    git pull
+    git checkout version-1.05
+    cd ..
+else
+    git clone --branch version-1.05 https://github.com/IATI/IATI-Codelists.git IATI-Codelists-105
+fi
+
 mkdir codelists
 cp IATI-Codelists-NonEmbedded/xml/*.xml codelists
 cp Unofficial-Codelists/xml/*.xml codelists
@@ -55,3 +64,25 @@ python src/v2tov1.py docs/.vuepress/public/api/clv2 docs/.vuepress/public/api/cl
 
 echo ' *** Copy Clv3 all formats to root ***'
 cp -r docs/.vuepress/public/api/clv3/* docs/.vuepress/public/api
+
+###################################################################
+
+echo ' *** v1.05: Copy source XML to CLv3 ***'
+mkdir -p docs/.vuepress/public/api/105/clv3/xml docs/.vuepress/public/api/105/clv2/xml
+cp IATI-Codelists-105/xml/*.xml docs/.vuepress/public/api/105/clv3/xml
+
+echo ' *** v1.05: Convert CLv3 XML to CLv2 XML ***'
+for f in IATI-Codelists-105/xml/*.xml; do
+    python src/v3tov2.py $f > docs/.vuepress/public/api/105/clv2/xml/`basename $f`;
+done
+echo ' *** v1.05: Generate CLv3 other formats (CSV; JSON; XLSX) ***'
+python src/gen.py docs/.vuepress/public/api/105/clv3
+
+echo ' *** v1.05: Copy CLv3 other formats (CSV; JSON; XLSX) to CLv2 ***'
+cp -r docs/.vuepress/public/api/105/clv3/{codelists.json,codelists.xml,csv,json,xlsx} docs/.vuepress/public/api/105/clv2/
+
+echo ' *** v1.05: Convert CLv2 all formats to CLv1 ***'
+python src/v2tov1.py docs/.vuepress/public/api/105/clv2 docs/.vuepress/public/api/105/clv1
+
+echo ' *** v1.05: Copy Clv3 all formats to root ***'
+cp -r docs/.vuepress/public/api/105/clv3/* docs/.vuepress/public/api/105
