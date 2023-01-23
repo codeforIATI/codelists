cfbff4276f4519934ac888999a2285bfe46d170d
Mark Brough <mark.brough@gmail.com>
2023-01-23 19:30:54 +0100

    Check if URL is valid

--- static/.vuepress/components/CodelistPage.vue
+++ static/.vuepress/components/CodelistPage.vue
@@ -85,6 +85,15 @@
         </div>
       </template>
 
+      <template v-slot:cell(url)="data">
+        <template v-if="isValidUrl(data.item.url)">
+          <a :href="data.item.url">{{ data.item.url }}</a>
+        </template>
+        <template v-else>
+          {{ data.item.url }}
+        </template>
+      </template>
+
       <template v-slot:cell(code)="data">
         <a :id="data.item.code" style="visibility:hidden;padding-top:73px;"></a>
         <router-link :to="'#' + data.item.code">{{ data.item.code }}</router-link>
@@ -220,6 +229,14 @@
       }
     },
     methods: {
+      isValidUrl(url) {
+        try {
+          new URL(url)
+          return true
+        } catch (err) {
+          return false
+        }
+      },
       onFiltered(filteredItems) {
         // Trigger pagination to update the number of buttons/pages due to filtering
         this.totalRows = filteredItems.length
